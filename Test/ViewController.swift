da220995d6a358def512fe125501314b27edc6aa
Lohen <17508663+lohenyumnam@users.noreply.github.com>
2019-01-23 12:43:34 +0530

    update

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MessageViewController
 
-class ViewController: UIViewController {
+class ViewController: MessageViewController {
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup(scrollView: tableView)
+        // Change the appearance of the text view and its content
+        messageView.inset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        messageView.textView.placeholderText = "New message..."
+        messageView.textView.placeholderTextColor = .lightGray
+        messageView.font = .systemFont(ofSize: 17)
+        messageView.backgroundColor = .yellow
+        self.borderColor = .red
+        
+        // Setup the button using text or an icon
+        messageView.setButton(title: "Send", for: .normal, position: .right)
+        messageView.rightButtonTint = .blue
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        messageView.layoutIfNeeded()
+        tableView.layoutIfNeeded()
+        self.view.layoutIfNeeded()
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
+
+
+
 
